b1c8410a7f3435a44ec34ebd5c331376f1a666e4
NAveeN4416 <naveenkumar3a5@gmail.com>
2021-05-18 14:32:41 +0530

    new file calc2.py added

---
+++ calc2.py
@@ -0,0 +1,20 @@
+#Version V1
+
+def multipler(m):
+	def calculate(n):
+ 		return m * n
+
+	return calculate
+
+def divider(m):
+	def calculate(n):
+ 		if n:
+ 			return m / n
+ 		return None
+	return calculate
+
+
+three_multipl = multipler(3)
+
+print(three_multipl(3))
+print(three_multipl(4))
