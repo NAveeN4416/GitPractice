f0b2cc91a1210f69a9108fe72733a40fe4f2f9b4
NAveeN4416 <naveenkumar3a5@gmail.com>
2021-05-20 10:06:18 +0530

    T457 code created with python classes

---
+++ T457.py
@@ -0,0 +1,18 @@
+#Ticket 457 Issue
+
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
+
+# Output: 10
+print(Person.age)
+
+# Output: <function Person.greet>
+print(Person.greet)
+
+# Output: "This is a person class"
+print(Person.__doc__)
